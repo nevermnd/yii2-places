38d0117067fefe20549e9fee0ced74913eb6c142
nevermnd <thiago.oliveira.gt14@gmail.com>
2015-12-29 02:31:28 -0200

    Added plugin events support.

--- src/PlacesAutocomplete.php
+++ src/PlacesAutocomplete.php
@@ -24,6 +24,10 @@ class PlacesAutocomplete extends InputWidget
      */
     public $typeaheadOptions = [];
     /**
+     * @var JsExpression|string On select plugin event handler
+     */
+    public $onSelect;
+    /**
      * @var array Default typeahead options
      */
     protected $defaultOptions = [
@@ -38,14 +42,55 @@ class PlacesAutocomplete extends InputWidget
         $view = $this->getView();
         PlacesPluginAsset::register($view);
 
-        $options = !empty($this->pluginOptions) ? Json::encode($this->pluginOptions) : '{}';
-        $typeAhead = ArrayHelper::merge($this->defaultOptions, $this->typeaheadOptions);
-        $typeAhead['source'] = new JsExpression("new AddressPicker($options).ttAdapter()");
-
-        $view->registerJs("$('#{$this->options['id']}').typeahead(null, " . Json::encode($typeAhead) . ");");
+        $script = implode("\n", [$this->buildAdapter(), $this->buildTypeAhead(), $this->buildEvent()]);
+        $view->registerJs($script);
 
         return $this->hasModel()
             ? Html::activeTextInput($this->model, $this->attribute, $this->options)
             : Html::textInput($this->name, $this->value, $this->options);
+    }
+
+    /**
+     * Build the required adapter JS
+     *
+     * @return string
+     */
+    protected function buildAdapter()
+    {
+        $options = !empty($this->pluginOptions) ? Json::encode($this->pluginOptions) : '{}';
+
+        return "var addressPicker = new AddressPicker($options);";
+    }
+
+    /**
+     * Build the required plugin event JS
+     *
+     * @return string
+     */
+    protected function buildEvent()
+    {
+        $js = '';
+        if (!$this->onSelect) {
+            return $js;
+        }
+
+        $event = $this->onSelect instanceof JsExpression ? $this->onSelect : new JsExpression($this->onSelect);
+        $js .= "addressPicker.bindDefaultTypeaheadEvent($('#{$this->options['id']}'));";
+        $js .= "$(addressPicker).on('addresspicker:selected', $event);";
+
+        return $js;
+    }
+
+    /**
+     * Build the typeahead initialization JS code
+     *
+     * @return string
+     */
+    protected function buildTypeAhead()
+    {
+        $options = ArrayHelper::merge($this->defaultOptions, $this->typeaheadOptions);
+        $options['source'] = 'addressPicker.ttAdapter()';
+
+        return "$('#{$this->options['id']}').typeahead(null, " . Json::encode($options) . ");";
     }
 }
